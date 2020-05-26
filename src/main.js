3648c0443e1fc9bca800736063775e0edb98035b
Jesse <dynaptx@163.com>
2020-05-26 22:31:11 +0800

    更新样式

--- src/main.js
+++ src/main.js
@@ -1,36 +1,64 @@
-const hashMap = [
-    {logo: 'A', url: 'https://www.acfun.cn'},
-    {logo: 'B', url:'https://www.bilibili.com'}
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
+const x = localStorage.getItem('x')
+const xObject = JSON.parse(x)
+
+const hashMap = xObject || [{
+    logo: 'A',
+    url: 'https://www.acfun.cn'
+  },
+  {
+    logo: 'B',
+    url: 'https://www.bilibili.com'
+  }
 ]
+
+const simplifyUrl = (url) => {
+  return url.replace('https://', '')
+    .replace('http://', '')
+    .replace('www.', '')
+    .replace(/\/.*/, '') // 删除 / 开头的内容
+}
+
 const render = () => {
-    $siteList.find('li:not(.last)').remove()
-    hashMap.forEach(node => {
-        const $li = $(`<li>
+  $siteList.find('li:not(.last)').remove()
+  hashMap.forEach(node => {
+    const $li = $(`<li>
         <a href="${node.url}">
             <div class="site">
                 <div class="logo">${node.logo[0]}</div>
-                <div class="link">${node.url}</div>
+                <div class="link">${simplifyUrl(node.url)}</div>
             </div>
         </a>     
       </li>`).insertBefore($lastLi)
-    })
+  })
 }
 
 render()
 
 $('.addButton').on('click', () => {
-    let url = window.prompt('请问要添加的网址是?')
+  let url = window.prompt('请问要添加的网址是?')
 
+  if (url.indexOf("http") !== 0) {
+    url = 'https://' + url
+  }
+  console.log(url)
 
-    if (url.indexOf('http') !== 0){
-        url = 'https://' + url
-    }
-    console.log(url)
-
-    hashMap.push({logo: url[0], url: url})
-
+  hashMap.push({
+    logo: simplifyUrl(url)[0].toUpperCase(),
+    url: url
+  })
+  render()
 
 })
+
+window.onbeforeunload = () => {
+  console.log('页面要关闭了')
+  const string = JSON.stringify(hashMap)
+
+  localStorage.setItem('x', string)
+}
+
 
 //
 // $('.addButton').on('click', () => {
