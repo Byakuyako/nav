25f16f22e1aca6f4b318f0a6cee2c81425ac06aa
Jesse <dynaptx@163.com>
2020-05-29 23:09:55 +0800

    完成PC端的样式处理

--- src/main.js
+++ src/main.js
@@ -1,84 +1,96 @@
-const $siteList = $('.siteList')
-const $lastLi = $siteList.find('li.last')
-const x = localStorage.getItem('x')
-const xObject = JSON.parse(x)
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
 
 const hashMap = xObject || [{
-    logo: 'A',
-    url: 'https://www.acfun.cn'
+    logo: "A",
+    url: "https://www.acfun.cn",
   },
   {
-    logo: 'B',
-    url: 'https://www.bilibili.com'
-  }
-]
+    logo: "B",
+    url: "https://www.bilibili.com",
+  },
+  {
+    logo: "G",
+    url: "https://github.com",
+  },
+];
 // 简化url
 const simplifyUrl = (url) => {
-  return url.replace('https://', '')
-    .replace('http://', '')
-    .replace('www.', '')
-    .replace(/\/.*/, '') // 删除 / 开头的内容
-}
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, ""); // 删除 / 开头的内容
+};
 
 const render = () => {
-  $siteList.find('li:not(.last)').remove()
-  hashMap.forEach(node => {
+  $siteList.find("li:not(.last)").remove();
+  hashMap.forEach((node, index) => { //forEach第一个参数是当前元素,第二个元素是下标
     const $li = $(`<li>
-        <a href="${node.url}">
-            <div class="site">
-                <div class="logo">${node.logo[0]}</div>
-                <div class="link">${simplifyUrl(node.url)}</div>
-            </div>
-        </a>     
-      </li>`).insertBefore($lastLi)
-  })
-}
+        <div class="site">
+          <div class="logo">${node.logo[0]}</div>
+          <div class="link">${simplifyUrl(node.url)}</div>
+          <div class="close">                   
+            <svg class="icon">
+              <use xlink:href="#icon-close"></use>
+            </svg>
+          </div>
+        </div>          
+      </li>`).insertBefore($lastLi);
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $('.close').on('mouseover', () => {
+      $('.cloase').css('visibility', 'visible')
+    })
 
-render()
+    $li.on("click", ".close", (e) => {
+      e.stopPropagation(); //阻止冒泡
+      console.log(hashMap)
+      hashMap.splice(index, 1)
+      render()
+    });
+  });
+};
 
-$('.addButton').on('click', () => {
-  let url = window.prompt('请问要添加的网址是?')
+render();
+
+$(".addButton").on("click", () => {
+  let url = window.prompt("请问要添加的网址是?");
 
   if (url.indexOf("http") !== 0) {
-    url = 'https://' + url
+    url = "https://" + url;
   }
-  console.log(url)
+  console.log(url);
 
   hashMap.push({
     logo: simplifyUrl(url)[0].toUpperCase(),
-    url: url
-  })
-  render()
+    url: url,
+  });
+  render();
+});
 
+
+//在离开页面前处理数据
+window.onbeforeunload = () => {
+  console.log("页面要关闭了");
+  const string = JSON.stringify(hashMap);
+
+  localStorage.setItem("x", string);
+};
+
+//绑定键盘事件
+$(document).on('keypress', (e) => {
+  const {
+    key
+  } = e
+  for (let i = 0; i < hashMap.length; i++) {
+    if (hashMap[i].logo.toLowerCase() === key) {
+      window.open(hashMap[i].url)
+    }
+  }
 })
 
-window.onbeforeunload = () => {
-  console.log('页面要关闭了')
-  const string = JSON.stringify(hashMap)
-
-  localStorage.setItem('x', string)
-}
-
-
-//
-// $('.addButton').on('click', () => {
-//     let url = window.prompt('请问要添加的网址是?')
-//
-//
-//     if (url.indexOf('http') !== 0){
-//         url = 'https://' + url
-//     }
-//     console.log(url)
-//
-//     const $siteList = $('.siteList')
-//     const $lastLi = $siteList.find('li.last') //^^^^li.last 是jq的api吧
-//     const $li = $(`<li>
-//         <a href="${url}">
-//             <div class="site">
-//                 <div class="logo">${url[0]}</div>
-//                 <div class="link">${url}</div>
-//             </div>
-//         </a>
-//       </li>`).insertBefore($lastLi)
-//
-// })
+//在点击搜索框时禁用键盘事件, 保证正常输入
